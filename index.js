8fce7b29e914bcf4188e218ff613d2f7135540ef
DmitrySharabin <dmitrysharabin@gmail.com>
2019-12-05 23:00:31 +0300

    Add comments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Don't delete and/or edit this file
+// Make your changes in the index.html, style.css, script.js
+
+// Add user code and styles
 import "./style.css";
 import "./script.js";
-
-// Make your changes in the index.html, style.css, script.js
