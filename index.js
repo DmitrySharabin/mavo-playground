2457a114396257a2fe1572967b82e6e663ed82f0
Dmitry Sharabin <dmitrysharabin@gmail.com>
2019-12-06 13:52:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Don't delete and/or edit this file
 // Make your changes in the index.html, style.css, script.js
 
-// Add user code and styles
+// Add user styles and scripts
 import "./style.css";
 import "./script.js";
